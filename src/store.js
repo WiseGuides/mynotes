579ca1882796d04463e43a0dd3bb94f33521fe44
Hal Helms <hal.helms@gmail.com>
2018-08-02 11:37:30 -0700

    02AUG-1137

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleWare } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import logger from 'redux-logger';
 
 import reducer from './reducers';
 
-const store = createStore( reducer, applyMiddleWare(promise(), thunk, logger) );
+const store = createStore( reducer, applyMiddleware(promise(), thunk, logger) );
 
 export default store;
